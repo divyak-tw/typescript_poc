f9f0dccbcbccd087ada02749708fcfd89d38deb0
Divya K <divya.k@thoughtworks.com>
2023-10-24 13:35:56 +0530

    Add concrete logic for form field validation

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,37 @@ function autoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
     return adjDescriptor;
 }
 
+//Validation
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+
+function validate(validatable : Validatable) {
+    let isValid = true;
+    if (validatable.required) {
+        isValid = isValid && validatable.value.toString().trim().length !== 0;
+    }
+    if (validatable.minLength != null && typeof validatable.value == 'string') {
+        isValid = isValid && validatable.value.trim().length >= validatable.minLength;
+    }
+    if (validatable.maxLength != null && typeof validatable.value == 'string') {
+        isValid = isValid && validatable.value.trim().length <= validatable.maxLength;
+    }
+    if (validatable.min != null && typeof validatable.value == 'number') {
+        isValid = isValid && validatable.value >= validatable.min;
+    }
+    if (validatable.max != null && typeof validatable.value == 'number') {
+        isValid = isValid && validatable.value <= validatable.max;
+    }
+    return isValid;
+}
+
+//Project Input class
 class ProjectInput {
 
     templateElement: HTMLTemplateElement;
@@ -39,9 +70,25 @@ class ProjectInput {
         const enteredDescription = this.descriptionInputElement.value;
         const enteredPeople = this.peopleInputElement.value;
 
-        if (enteredTitle.trim().length === 0 ||
-            enteredDescription.trim().length === 0 ||
-            enteredPeople.trim().length === 0) {
+        const titleValidatable : Validatable = {
+            value: enteredTitle,
+            required: true
+        };
+
+        const descriptorValidatable : Validatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5,
+        };
+
+        const peopleValidatable : Validatable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1,
+            max: 5,
+        };
+
+        if (!validate(titleValidatable) || !validate(descriptorValidatable) || !validate(peopleValidatable)) {
             alert('Invalid input');
             return;
         } else {
